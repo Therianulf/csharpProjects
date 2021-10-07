bad8f7fc008c63c78f361b374c7a7f2de6e4af72
BenLarson <ben.larson@creative-alternatives.org>
2021-10-07 12:09:56 -0700

    Initial check-in of module testConsole

---
+++ testConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace testConsole
+{
+    class Program
+    {
+
+        static int tickCount = 0;
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee();
+            myEmployee.Age = 32;
+            myEmployee.Name = "Ben";
+            Console.WriteLine("Hello World! I am "+myEmployee.Age+" years old TickCount = " + tickCount.ToString());
+            tickCount++;
+            Console.WriteLine("Hello World! its me "+ myEmployee.Name +". TickCount = " + tickCount.ToString());
+            XmlSerialization.WriteToXmlFile<Employee>("/Users/ben.larson/employee.txt", myEmployee);
+        }
+    }
+
+
+    public class Employee {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
+
+}
