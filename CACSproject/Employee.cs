c1a3bcd7a5a7ea4347e82347b7adfff0d749c843
Benjamin Larson <therianlarson@gmail.com>
2021-10-07 21:41:30 -0700

    updating soulation and all namespace to new namespace

---
+++ CACSproject/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CACSproject
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Title { get; set; }
+        public float Salary { get; set; }
+        public void IncreaseSalary()
+        {
+            Salary = Salary + Salary * 0.08F;
+        }
+    }
+}
